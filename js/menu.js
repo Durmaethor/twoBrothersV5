1930eb9f02ce1ef5c618335bbf004c262462d9f1
Ryan Kramer <rkramer613@gmail.com>
2017-08-27 15:08:17 -0700

    Menu button tweaks and fixes

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,23 @@
+
 $(document).ready(function () {
     
-        $(".menu-btn a").click(function () {
+        $(".btn-menu a").click(function () {
             $(".overlay").fadeToggle(200);
             $(this).toggleClass('btn-open').toggleClass('btn-close');
         });
     
         $('.overlay').on('click', function () {
             $(".overlay").fadeToggle(200);
-            $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+            $(".btn-menu a").toggleClass('btn-open').toggleClass('btn-close');
         });
     
         $('.menu a').on('click', function () {
             $(".overlay").fadeToggle(200);
-            $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+            $(".btn-menu a").toggleClass('btn-open').toggleClass('btn-close');
         });
     
     });
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
